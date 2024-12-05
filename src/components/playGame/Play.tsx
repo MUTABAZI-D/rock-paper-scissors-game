036ba41aa458d34c6704b72e11d905b4581d1d89
MUTABAZI Dieudonne <mudonne18@gmail.com>
2024-12-05 20:16:44 +0200

    added framer motion for styling

--- src/components/playGame/Play.tsx
+++ src/components/playGame/Play.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { generateComputerChoice } from "../../store/gameSlice";
 import { AppDispatch } from "../../store/Store";
+import { motion } from "motion/react";
 
 type PlayProps = {
   setPlayerChoice: React.Dispatch<React.SetStateAction<string | null>>;
@@ -18,51 +19,58 @@ export const Play = ({ setPlayerChoice }: PlayProps) => {
 
   return (
     <Stack direction="row" spacing={2} mt={3}>
-      <Avatar
-        alt="rock"
-        src={"/images/rock-emoji.png"}
-        sx={{
-          width: { xs: 80, md: 116 },
-          height: { xs: 80, md: 116 },
-          border: "3px solid white",
-          "& img": {
-            width: { xs: 40, md: 56 },
-            height: { xs: 40, md: 56 },
-            mb: { xs: 1, md: 2 },
-          },
-        }}
-        onClick={() => handlePlayerChoice("rock")}
-      />
-
-      <Avatar
-        alt="paper"
-        src={"/images/paper-emoji.png"}
-        sx={{
-          width: { xs: 80, md: 116 },
-          height: { xs: 80, md: 116 },
-          border: "3px solid white",
-          "& img": {
-            width: { xs: 40, md: 56 },
-            height: { xs: 40, md: 56 },
-          },
-        }}
-        onClick={() => handlePlayerChoice("paper")}
-      />
-
-      <Avatar
-        alt="scissors"
-        src={"/images/scissors-emoji.png"}
-        sx={{
-          width: { xs: 80, md: 116 },
-          height: { xs: 80, md: 116 },
-          border: "3px solid white",
-          "& img": {
-            width: { xs: 40, md: 56 },
-            height: { xs: 40, md: 56 },
-          },
-        }}
-        onClick={() => handlePlayerChoice("scissors")}
-      />
+      <motion.div whileTap={{ scale: 1.1, opacity: 0.5 }}>
+        <Avatar
+          alt="rock"
+          src={"/images/rock-emoji.png"}
+          sx={{
+            cursor: "pointer",
+            width: { xs: 80, md: 116 },
+            height: { xs: 80, md: 116 },
+            border: "3px solid white",
+            "& img": {
+              width: { xs: 40, md: 56 },
+              height: { xs: 40, md: 56 },
+              mb: { xs: 1, md: 2 },
+            },
+          }}
+          onClick={() => handlePlayerChoice("rock")}
+        />
+      </motion.div>
+      <motion.div whileTap={{ scale: 1.1, opacity: 0.5 }}>
+        <Avatar
+          alt="paper"
+          src={"/images/paper-emoji.png"}
+          sx={{
+            cursor: "pointer",
+            width: { xs: 80, md: 116 },
+            height: { xs: 80, md: 116 },
+            border: "3px solid white",
+            "& img": {
+              width: { xs: 40, md: 56 },
+              height: { xs: 40, md: 56 },
+            },
+          }}
+          onClick={() => handlePlayerChoice("paper")}
+        />
+      </motion.div>
+      <motion.div whileTap={{ scale: 1.1, opacity: 0.5 }}>
+        <Avatar
+          alt="scissors"
+          src={"/images/scissors-emoji.png"}
+          sx={{
+            cursor: "pointer",
+            width: { xs: 80, md: 116 },
+            height: { xs: 80, md: 116 },
+            border: "3px solid white",
+            "& img": {
+              width: { xs: 40, md: 56 },
+              height: { xs: 40, md: 56 },
+            },
+          }}
+          onClick={() => handlePlayerChoice("scissors")}
+        />
+      </motion.div>
     </Stack>
   );
 };
